4b5445b11a405f0aa068ba7a3072e4e08206d612
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-22 14:33:45 +0100

    a finir

--- public/js/carrou.js
+++ public/js/carrou.js
@@ -1,40 +1,27 @@
 document.body.onload=function (){
-    n = 2
+
+    n = tab.length
     p = 0;
     container=document.getElementById("container");
     g=document.getElementById("g");
     d=document.getElementById("d");
     container.style.width=(800*n)+"px";
-    let tab = ["./public/images/projets/SuperBreaker/Menu_Super_Breaker.png","./public/images/projets/SpaceInvader/Menu_Space_Invader.png"];
     for(let image of tab){
         div = document.createElement("div");
         div.className="photo";
         div.style.backgroundImage="url("+image+")";
         container.appendChild(div);
     }
-    afficherMasquer();
     g.onclick=function (){
-        if (p<0)
-            p++;
-        container.style.transform="translate("+p*800+"px)";
-        container.style.transition="all 0.5s ease"
-        afficherMasquer();
+        p = (p+1)%n;
+        moove();
     }
     d.onclick = function () {
-        if (p>-n+1)
-            p--;
+        p = (p-1)%n;
+        moove();
+    }
+    function moove(){
         container.style.transform = "translate(" + p * 800 + "px)";
         container.style.transition="all 0.5s ease"
-        afficherMasquer();
-    }
-    function afficherMasquer(){
-        if (p==-n+1)
-            d.style.visibility="hidden"
-        else
-            d.style.visibility="visible"
-        if (p == 0)
-            g.style.visibility="hidden"
-        else
-            g.style.visibility="visible"
     }
 }
