7a15591fa11f1870d2bc7e80e2a0f3ff0ae77fad
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-29 11:28:18 +0100

    update couressel

--- public/js/carrou.js
+++ public/js/carrou.js
@@ -1,27 +1,38 @@
-document.body.onload=function (){
-
+document.body.onload = function () {
+    function clicGauche() {
+        p = (p + 1) % n;
+        if (p === 1) {
+            p -= n;
+        }
+        moove();
+    }
+    function clicDroit() {
+        p = (p - 1) % n;
+        moove();
+    }
     n = tab.length
-    p = 0;
-    container=document.getElementById("container");
-    g=document.getElementById("g");
-    d=document.getElementById("d");
-    container.style.width=(800*n)+"px";
-    for(let image of tab){
+    p = index;
+
+    container = document.getElementById("container");
+
+    g = document.getElementById("g");
+    d = document.getElementById("d");
+
+    container.style.width = (800 * n) + "px";
+    for (let image of tab) {
         div = document.createElement("div");
-        div.className="photo";
-        div.style.backgroundImage="url("+image+")";
+        div.className = "photo";
+        div.style.backgroundImage = "url(" + image + ")";
         container.appendChild(div);
     }
-    g.onclick=function (){
-        p = (p+1)%n;
-        moove();
+    g.onclick = function () {
+        clicGauche()
     }
     d.onclick = function () {
-        p = (p-1)%n;
-        moove();
+        clicDroit()
     }
-    function moove(){
+    function moove() {
         container.style.transform = "translate(" + p * 800 + "px)";
-        container.style.transition="all 0.5s ease"
+        container.style.transition = "all 0.5s ease"
     }
 }
